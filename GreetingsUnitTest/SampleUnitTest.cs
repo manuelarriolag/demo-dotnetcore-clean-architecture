d36ed810f315ded2fcf59f7cdf9ff665b7bd65e1
Manuel Arriola <32366746+manuelarriolag@users.noreply.github.com>
2026-03-09 17:45:45 -0600

    test: replace LocalDB with SQLite in-memory for cross-platform runs

--- GreetingsUnitTest/SampleUnitTest.cs
+++ GreetingsUnitTest/SampleUnitTest.cs
@@ -1,25 +1,41 @@
 using GreetingsCore.Adapters.Db;
 using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
+using Microsoft.Data.Sqlite;
 
 namespace GreetingsUnitTest
 {
     public class Tests
     {
         private GreetingContext _context;
+        private SqliteConnection _connection;
 
         [SetUp]
         public void Setup()
         {
-            var options = new DbContextOptionsBuilder<GreetingContext>().Options;
-            _context = new GreetingContext(options);            
+            _connection = new SqliteConnection("Data Source=:memory:");
+            _connection.Open();
+
+            var options = new DbContextOptionsBuilder<GreetingContext>()
+                .UseSqlite(_connection)
+                .Options;
+
+            _context = new GreetingContext(options);
+            _context.Database.EnsureCreated();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _context?.Dispose();
+            _connection?.Dispose();
         }
 
         [Test]
-        public void CanConnnectToDataBase_ReturnFalse()
+        public void CanConnnectToDataBase_ReturnTrue()
         {
-            var isConnected =  _context.Database.CanConnect();
-            Assert.IsFalse(isConnected, "It can't connect with data base.");
+            var isConnected = _context.Database.CanConnect();
+            Assert.IsTrue(isConnected, "It should connect with in-memory SQLite database.");
         }
 
     }
